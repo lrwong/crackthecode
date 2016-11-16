aaae1726d8062c0a5721ddaaabdc728681ebb487
Leland Wong <lelandw@lelandw-m01.vmware.com>
2016-11-16 10:08:39 -0800

    fixed 1.1_no_hash.js, also now accepts arguments from the command line

--- part1/ch1/1.1_no_hash.js
+++ part1/ch1/1.1_no_hash.js
@@ -1,8 +1,15 @@
+"use strict";
+
+let argv = require('yargs')
+.usage('Usage: node $0 --str ["str"]')
+.demand(['str'])
+.argv;
+
 function unique(str){
      let j = 0;
      for(let i = 0; i < str.length; i++){
           let x = str.charAt(i);
-          j = i;
+          j = i+1;
           while(j < str.length){
                if(x === str.charAt(j)){
                     return false;
@@ -12,3 +19,5 @@ function unique(str){
      }
      return true;
 }
+
+console.log(unique(argv.str));
